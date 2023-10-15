12adece3b580655f3e6513673ca08a0853ce67f5
Elian Emir Hadad <hadad.elian@gmail.com>
2023-10-14 21:12:14 -0300

    modifica data

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,6 +2,15 @@ export const paginas = [
   {
     id: '1',
     imagen:
+      'https://firebasestorage.googleapis.com/v0/b/portfoliodev-3f25b.appspot.com/o/imageweb%2FllTask.png?alt=media&token=b1a95974-d082-41e9-8807-8af54d2ec249',
+    url_github: 'https://github.com/Elianhad/mern-task-socket',
+    url_web: 'https://iitask.vercel.app/',
+    nombre: 'IItask - Gestión de proyectos ',
+    tech: ['MERN', 'Socket.io', 'Tailwindcss-HeadlessUI']
+  },
+  {
+    id: '2',
+    imagen:
       'https://firebasestorage.googleapis.com/v0/b/portfoliodev-3f25b.appspot.com/o/imageweb%2FFrontend%20Mentor%20News%20homepage.png?alt=media&token=2b76dd87-ba70-45e0-bd4b-9c63dacd09a2',
     url_github: 'https://github.com/Elianhad/frontendmentor-news/',
     url_web: 'https://fontendmentor-news-eeh.netlify.app/',
@@ -9,7 +18,7 @@ export const paginas = [
     tech: ['ViteJs', 'ReactJs']
   },
   {
-    id: '02',
+    id: '3',
     imagen:
       'https://firebasestorage.googleapis.com/v0/b/portfoliodev-3f25b.appspot.com/o/imageweb%2Fmidubooks%20page.png?alt=media&token=241445a1-75c4-4d74-ab9e-2b0377cc572c',
     url_github:
