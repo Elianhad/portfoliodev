6788a35f2c01fd48edfb2053ea5f41a00472a197
Elian Emir Hadad <hadad.elian@gmail.com>
2024-04-13 15:03:26 -0300

    cambia hero, agrega video

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ import animations from '@midudev/tailwind-animations'
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'hero-bg':"url('https://res.cloudinary.com/dksibcbrj/image/upload/v1713016477/portfolio/219ce3d40f6289c3186a5ba150ad3f4d_ahtk18.jpg')",
+			},
+		},
 	},
 	plugins: [animations],
 }
